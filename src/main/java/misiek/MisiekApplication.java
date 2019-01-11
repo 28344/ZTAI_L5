c073401398afca8491846906aad00450348c42af
28344 <miskoo96@gmail.com>
2019-01-11 15:58:03 +0100

    spring boot

---
+++ src/main/java/misiek/MisiekApplication.java
@@ -0,0 +1,13 @@
+package misiek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MisiekApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MisiekApplication.class, args);
+    }
+
+}
---
+++ src/test/java/misiek/misiek/MisiekApplicationTests.java
@@ -0,0 +1,17 @@
+package misiek.misiek;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MisiekApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
